43525e179a500b6c62b62451c7940943279d6d67
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-11-29 15:26:19 -0300

    correção database

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-// console.log(process.env.CONNECTIONSTRING)
-// mongoose.connect(process.env.CONNECTIONSTRING,{ useNewUrlParser: true }).then(()=>{
-//     console.log('Conectei a base de dados')
-// })
+console.log(process.env.CONNECTIONSTRING)
+mongoose.connect(process.env.CONNECTIONSTRING,{ useNewUrlParser: true }).then(()=>{
+    console.log('Conectei a base de dados')
+})
 
-mongoose.connect('mongodb://localhost/my-english')
+// mongoose.connect('mongodb://localhost/my-english')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
